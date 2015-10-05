34d0e221eef042316f52e8c2ff7b5094472483ea
zomars <zomars@me.com>
2015-10-05 08:17:28 -0600

    Add lesson 2 exercises

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -35,9 +35,52 @@ work.city = "Culiacán";
 
 var education = {};
 
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+    $("#skills").append(formattedSkill);
+}
+
+for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmpoyerTitle = formattedEmployer + " – " + formattedTitle;
+
+    $(".work-enty:last").append(formattedEmpoyerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-enty:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-enty:last").append(formattedDescription);
+}
+
 $("#header").append(bio.name);
 $("#header").append(bio.role);
 $("#header").append(bio.contact);
 $("#header").append(bio.picture);
 $("#header").append(bio.welcome);
 $("#header").append(bio.skills);
+
+$("#main").append(internationalizeButton);
+
+function inName(){
+
+    nameArray = name.trim().split(" ");
+    console.log(nameArray);
+
+    nameArray[1] = nameArray[1].toUpperCase();
+    nameArray[0] = nameArray[0].slice(0, 1).toUpperCase() + nameArray[0].slice(1).toLowerCase();
+
+    return nameArray[0] + " " + nameArray[1];
+}
+
+
